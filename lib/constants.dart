817af6d01b10a7ab4c5f790fa62ecab05b849df7
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-02-09 16:06:44 +0530

    Created and updated: chat screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-const kSendButtonTextStyle = TextStyle(
+const SendButtonTextStyle = TextStyle(
   color: Colors.lightBlueAccent,
   fontWeight: FontWeight.bold,
   fontSize: 18.0,
 );
 
-const kMessageTextFieldDecoration = InputDecoration(
+const MessageTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   hintText: 'Type your message here...',
   border: InputBorder.none,
 );
 
-const kMessageContainerDecoration = BoxDecoration(
+const MessageContainerDecoration = BoxDecoration(
   border: Border(
     top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
   ),
